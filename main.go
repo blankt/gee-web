b49f93b1514703eaad964775b48eecd44e5a0b79
lemon <534464210@qq.com>
2022-03-28 23:10:38 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL,PATH=%q\n", req.URL.Path)
+	case "/heelo":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
+
+func main() {
+	engine := new(Engine)
+	http.ListenAndServe(":8080", engine)
+}
